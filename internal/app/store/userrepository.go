ce31ef2a1cd90ec2c25ac3d08eb99d71b8741952
Alexey Ovsyannikov <AOvsyannikov2@alfabank.ru>
2021-09-22 00:53:27 +0300

    Added user repository

---
+++ internal/app/store/userrepository.go
@@ -0,0 +1,25 @@
+package store
+
+import (
+	"github.com/AlexeyInc/Golang_WebAPI/internal/model"
+)
+
+type UserRepository struct {
+	store *Store
+}
+
+func (r *UserRepository) Create(u *model.User) (*model.User, error) {
+	if err := r.store.db.QueryRow(
+		"INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id",
+		u.Email,
+		u.EncryptedPassword,
+	).Scan(&u.ID); err != nil {
+		return nil, err
+	}
+
+	return u, nil
+}
+
+func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
+	return nil, nil
+}
